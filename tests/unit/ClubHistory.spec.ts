caa57a513fc15cb6de6eae756291cdff0cbe7d68
Ewan Bauld <ewan.bauld@gmail.com>
2018-09-10 18:59:13 +0100

    Fix test

--- tests/unit/ClubHistory.spec.ts
+++ tests/unit/ClubHistory.spec.ts
@@ -1,5 +1,5 @@
 import { shallowMount } from '@vue/test-utils'
-import ClubHistory from '@/views/K`endoVideo.vue'
+import ClubHistory from '@/views/ClubHistory.vue'
 
 describe('ClubHistory.vue', () => {
   it('renders correctly', () => {
