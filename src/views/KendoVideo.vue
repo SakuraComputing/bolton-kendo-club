960ed5edd0f4deac68a6879f7f760f7d91a20f70
Ewan Bauld <ewan.bauld@gmail.com>
2018-09-12 00:03:11 +0100

    Add Video functionality

--- src/views/KendoVideo.vue
+++ src/views/KendoVideo.vue
@@ -1,10 +1,36 @@
 <template>
     <div class="kendo-video">
         <h1>Kendo Video</h1>
-        <p>Need to add in favourite videos</p>
-        <youtube video-id="LHNPoSBJvn8" />
+        <youtube 
+            v-for="video in videos"
+            :video-id="video.videoId"
+            :key="video.etag"
+        />
     </div>    
 </template>
+
+<script>
+    import axios from 'axios';
+
+    export default {
+    data() {
+        return {
+            videos: []
+        }
+    },
+    created: function() {
+        this.fetchVideos();
+    },
+    methods: {
+        fetchVideos() {
+            let uri = 'http://localhost:5000/api/club/Bolton Kendo Club';
+            axios.get(uri).then((response) => {
+                this.videos = response.data.videos
+            });
+        }
+    }
+}
+</script>
 
 <style>
 
