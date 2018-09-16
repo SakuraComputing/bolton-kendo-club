0c9a907c78fee5a56e4f960a31010e925280886e
Ewan Bauld <ewan.bauld@gmail.com>
2018-09-16 23:06:39 +0100

    Add vue2 maps in

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,18 @@ import router from './router'
 import store from './store'
 import './registerServiceWorker'
 import VueYoutube from 'vue-youtube'
+import * as VueGoogleMaps from 'vue2-google-maps';
 
 Vue.config.productionTip = false
 
 Vue.use(VueYoutube)
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyADAT64SMWOP7ee4cEOgW8tMYNL673MeDQ',
+    libraries: "places"
+  }
+})
 
 new Vue({
   router,
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,30 +1,45 @@
 <template>
 
   <div class="about-container">
-      <div class="about-section">
-        <h2>Where we practise</h2>
-        <ul class="address-list" v-for="address in club.addresses" :key="address.postcode">
-            <div>{{ address.address1 }}</div>
-            <div>{{ address.address2 }}</div>
-            <div>{{ address.address3 }}</div>
-            <div>{{ address.address4 }}</div>
-            <div>{{ address.postcode }}</div>
-        </ul>
-      </div>
-      <div class="about-section">
-        <h2>Practise Times</h2>
-        <ul>    
-            <li class="about-list" v-for="practice in club.practices" :key="practice.venue">
-                <div>
-                    Venue: {{ practice.venue }}
-                </div>
-                <div>
-                    Time: {{ practice.time }}
-                </div>
-            </li>
-        </ul>
+        <div class="about-section map-container">
+            <h2>Where we practise</h2>
+            <ul class="address-list" v-for="address in club.addresses" :key="address.postcode">
+                <div>{{ address.address1 }}</div>
+                <div>{{ address.address2 }}</div>
+                <div>{{ address.address3 }}</div>
+                <div>{{ address.address4 }}</div>
+                <div>{{ address.postcode }}</div>
+            </ul>
+            <div class="map-box">
+                <br>
+                <gmap-map
+                    :center="{ lat: 53.562614, lng: -2.384833 }"
+                    :zoom="15"
+                    style="width:100%;  height: 400px;"
+                >
+                <gmap-marker
+                    :key="index"
+                    v-for="(m, index) in markers"
+                    :position="m.position"
+                    @click="center=m.position"
+                ></gmap-marker>
+                </gmap-map>
+            </div>
+        </div>
+        <div class="about-section">
+            <h2>Practise Times</h2>
+            <ul>    
+                <li class="about-list" v-for="practice in club.practices" :key="practice.venue">
+                    <div>
+                        Venue: {{ practice.venue }}
+                    </div>
+                    <div>
+                        Time: {{ practice.time }}
+                    </div>
+                </li>
+            </ul>
 
-      </div>
+        </div>
       <div class="about-section">
         <h2>Who to Contact</h2>
         <ul>
@@ -51,10 +66,21 @@
 import axios from 'axios';
 
 export default {
+    name: "GoogleMap",
     data() {
         return {
-            club: ''        
+            club: '',      
+             // default to Montreal to keep it simple
+            // change this to whatever makes sense
+            
+            center: { lat: 53.562614, lng: -2.384833 },
+            markers: [  { position:{ lat: 53.562614, lng: -2.384833 }}],
+            places: [],
+            currentPlace: null
         }
+    },
+    mounted() {
+        this.geolocate();
     },
     created: function() {
         this.fetchClubDetails();
@@ -64,6 +90,30 @@ export default {
             let uri = 'http://localhost:5000/api/club/Bolton Kendo Club';
             axios.get(uri).then((response) => {
                 this.club = response.data
+            });
+        },
+        // receives a place object via the autocomplete component
+        setPlace(place) {
+            this.currentPlace = place;
+        },
+        addMarker() {
+            if (this.currentPlace) {
+                const marker = {
+                lat: this.currentPlace.geometry.location.lat(),
+                lng: this.currentPlace.geometry.location.lng()
+                };
+                this.markers.push({ position: marker });
+                this.places.push(this.currentPlace);
+                this.center = marker;
+                this.currentPlace = null;
+            }
+        },
+        geolocate: function() {
+            navigator.geolocation.getCurrentPosition(position => {
+                this.center = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+                };
             });
         }
     }
@@ -86,6 +136,10 @@ export default {
 
 .address-list {
     list-style-type: none;
+}
+
+.map-container {
+    /* display: inline; */
 }
 
 .about-list {
