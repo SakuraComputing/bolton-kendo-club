34616c0262dbaa0071d0b4c917946debbac974e5
Ewan Bauld <ewan.bauld@gmail.com>
2018-10-05 14:59:29 +0100

    Create production build

---
+++ dist/precache-manifest.f18ecc91f7258bce2cee8afffffdd375.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "d06b49ac365e03297fd0",
+    "url": "/css/chunk-9203.aee57af2.css"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "d06b49ac365e03297fd0",
+    "url": "/js/chunk-9203.6abde327.js"
+  },
+  {
+    "revision": "904360fcc58cedba621b",
+    "url": "/css/app.7317c401.css"
+  },
+  {
+    "revision": "074799cca6871414d5b6",
+    "url": "/js/chunk-0a3d.d33b0f9d.js"
+  },
+  {
+    "revision": "f4037a404a7c130e4fa0",
+    "url": "/css/chunk-3c45.33976ccd.css"
+  },
+  {
+    "revision": "f4037a404a7c130e4fa0",
+    "url": "/js/chunk-3c45.0665832a.js"
+  },
+  {
+    "revision": "3542ec0730767dba2379",
+    "url": "/css/chunk-7684.b9dd8523.css"
+  },
+  {
+    "revision": "3542ec0730767dba2379",
+    "url": "/js/chunk-7684.a8517883.js"
+  },
+  {
+    "revision": "3a6acb3eb3c5c3c8f2fe",
+    "url": "/css/chunk-89ae.118edd03.css"
+  },
+  {
+    "revision": "3a6acb3eb3c5c3c8f2fe",
+    "url": "/js/chunk-89ae.1955a56d.js"
+  },
+  {
+    "revision": "53dce376680afc60bf0d",
+    "url": "/js/about.92b64337.js"
+  },
+  {
+    "revision": "904360fcc58cedba621b",
+    "url": "/js/app.0649ddd0.js"
+  },
+  {
+    "revision": "ec77f41c034ec7139642",
+    "url": "/css/chunk-aa75.3788b602.css"
+  },
+  {
+    "revision": "ec77f41c034ec7139642",
+    "url": "/js/chunk-aa75.95ecdf83.js"
+  },
+  {
+    "revision": "f738b8a142a573737f26",
+    "url": "/css/chunk-e8e7.22262670.css"
+  },
+  {
+    "revision": "f738b8a142a573737f26",
+    "url": "/js/chunk-e8e7.f0d98ae0.js"
+  },
+  {
+    "revision": "f9038ca3f8aac06b6a72",
+    "url": "/css/chunk-vendors.526d22f1.css"
+  },
+  {
+    "revision": "f9038ca3f8aac06b6a72",
+    "url": "/js/chunk-vendors.d5d36843.js"
+  },
+  {
+    "revision": "9d4679241f833bde83934be2cbf07e48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb9c64f6709e8f1124186fd6a06ed7e2",
+    "url": "/img/background/kyoto-taikai.jpg"
+  },
+  {
+    "revision": "53dce376680afc60bf0d",
+    "url": "/css/about.b0815b2d.css"
+  }
+];
--- tests/unit/About.spec.ts
+++ tests/unit/About.spec.ts
@@ -1,8 +1,17 @@
 import { shallowMount } from '@vue/test-utils';
 import About from '@/views/About.vue';
+const globalAny:any = global;
 
 describe('About.vue', () => {
   it('renders correctly', () => {
+    const mockGeolocation = {
+      getCurrentPosition: jest.fn(),
+      watchPosition: jest.fn()
+    };
+
+    
+    globalAny.navigator.geolocation = mockGeolocation;
+
     const wrapper = shallowMount(About);
     expect(wrapper).toMatchSnapshot()
   })
