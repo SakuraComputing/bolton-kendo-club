904f4312261ed954a36306ecf0f8eca284b8cc41
Ewan Bauld <ewan.bauld@gmail.com>
2018-10-05 17:46:08 +0100

    Update server.js to run api on the same port

--- server.js
+++ server.js
@@ -1,7 +1,45 @@
 const express = require('express');
-const port = process.env.PORT || 8080;
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+
+const users = require('./api/users');
+const club = require('./api/club');
 
 const app = express();
+
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to Mongo DB
+mongoose
+    .connect(db)
+    .then(() => console.log('Mongo DB connected'))
+    .catch(err => console.log(err));
+
+app.use(passport.initialize());
+
+// Passport Config
+require('./config/passport')(passport);
+
+// Allow CORS
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+// Use Routes 
+app.use('/api/users', users);
+app.use('/api/club', club);
+
+
+const port = process.env.PORT || 8080;
+
 
 app.use(express.static(__dirname + "/dist/"));
 app.get(/.*/, function(req, res) {
