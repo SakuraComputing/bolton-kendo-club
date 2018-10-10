0925ebd70be94800a2df2cc00bd60da7535d783b
Ewan Bauld <ewan.bauld@gmail.com>
2018-10-10 14:52:41 +0100

    Add Content Type to header to load service worker

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ require('./config/passport')(passport);
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Content-Type', 'application/javascript');
     next();
 });
 
