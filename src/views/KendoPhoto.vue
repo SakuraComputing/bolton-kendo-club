94426fc2b31a71f1fe3db8d7cbb49314c9521e37
Ewan Bauld <ewan.bauld@gmail.com>
2018-11-02 13:48:38 +0000

    Clean up photo code

--- src/views/KendoPhoto.vue
+++ src/views/KendoPhoto.vue
@@ -4,6 +4,7 @@
         <div>1. Kyoto Taikai</div>
         <div class="image">
             <img v-for="img in images" v-bind:src="img" v-bind:key="img" alt="No image">
+            <!-- <p v-for="text in images" :key="img></p> -->
         </div>
         <div>2. Wilf Swindell's sensei founder KNMS</div>
     </div>    
@@ -13,7 +14,16 @@
     export default {
         data() {
             return {
-                images:['/img/photos/2016-kyoto-taikai.png','/img/photos/Wilf Swindells Sensei.jpg']
+                images: [
+                    {
+                        img: '/img/photos/2016-kyoto-taikai.png',
+                        text: 'Kyoto Taikai'
+                    },
+                    {
+                        img: '/img/photos/Wilf Swindells Sensei.jpg',
+                        text: 'Wilf Swindells Sensei founder KNMS and northern Kendo in the UK'
+                    },
+                ]
             }
         }
     }
